93d5b762c205cc6f18d536a2acbca4856eb394ad
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2024-06-17 16:57:50 +0500

    Initial Commit

---
+++ RecipesSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipesSwiftUI
+//
+//  Created by Томирис Рахымжан on 17/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipesSwiftUI/RecipesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesSwiftUIApp.swift
+//  RecipesSwiftUI
+//
+//  Created by Томирис Рахымжан on 17/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
